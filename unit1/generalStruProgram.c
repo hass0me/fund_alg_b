c38646079e85581aeebc4d19fc9bad40bacaa0ca
hass0me <cescor.0503@gmail.com>
2022-01-29 20:57:51 -0600

    created some variables

---
+++ unit1/generalStruProgram.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//* Global Variables *;
+//Create a variable: dataType nameVariable = anyData;
+//dataType in C/C++/C#/PHP
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "CesarCorrea";
+char unCaracter = 'M';
+//////////////////////////////////////
+
+
+/// Laa ejecucion del programa//
+int main (){
+    printf("Hello Data B %s\n", name)
+    return 0;
+}
