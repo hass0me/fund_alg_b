f333608aafa72c4609cf16b50918230ddc0a138a
hass0me <cescor.0503@gmail.com>
2022-02-10 18:45:23 -0600

    new file

---
+++ unit1/VariablesGlobalLocal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//*Global variables*//
+char* date= "02-02-22T10:25:00z";
+/////////////////////////////////////
+
+/// La ejecucion del programa/////
+int main (){
+   date = "02-03-22T10:25:00z";
+   float pi = 3.1416 //%c
+    char character = "c"; //%c
+    char* name = "cesar"; //%
+    int *adress = &hours; //%p
+    hours = hours +2;
+    printf("valor")
+    return 0;
+}
+////////////////////////////////
